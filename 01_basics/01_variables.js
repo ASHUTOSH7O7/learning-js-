70c040a1202345e03107f8cf31298a2ebe6fe8cd
ASHUTOSH7O7 <128122519+ASHUTOSH7O7@users.noreply.github.com>
2025-07-17 18:29:11 +0000

    variables info

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 87367
+let accountEmail = "ashutoshgoogle.com"
+var accountPassword = "12344"
+accountCity = "Lucknow"
+
+
+console.log(accountId);
+
+accountEmail = "as@as.com"
+accountPassword = "8737"
+accountCity = "mumbai"
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+      
