86d28d5ef543af491e1ab80f7e755fe8e0499ebe
ASHUTOSH7O7 <128122519+ASHUTOSH7O7@users.noreply.github.com>
2025-07-17 20:20:45 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+//let value = 3
+//let negvalue = -value
+//console.log(negvalue);
+let name = "ashutosh"
+//let score = false
+let isLoggdIn = false
+
+
+
+
+//console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
